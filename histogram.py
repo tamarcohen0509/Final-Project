334a19d02c200b6b6c4a52c9216c1d81e43bf338
Tamar Cohen <tamarc456@gmail.com>
2020-06-15 18:50:10 +0300

    create 1d histogram - cont.

--- histogram.py
+++ histogram.py
@@ -1,12 +1,40 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+from geopy import distance
 
-def sub_plots(table, attr1, attr2):
+def get_min_point_on_axis(data_frame, axis):
+    sample = (data_frame.loc[data_frame[axis].idxmin()])
+    print(sample['latitude'], sample['longitude'])
+    return sample['latitude'], sample['longitude']
+
+
+def get_max_point_on_axis(data_frame, axis):
+    sample = (data_frame.loc[data_frame[axis].idxmax()])
+    print(sample['latitude'], sample['longitude'])
+    return sample['latitude'], sample['longitude']
+
+
+def get_range(min, max):
+    # normalize the points to the same axis value
+    print(min[1],max[1])
+    dist = distance.distance(min, max).km
+    return dist
+    # print(distance.distance(min, max).km)
+
+
+# get number of equal-width bins in the range
+def get_bins(range):
+    return range/0.001
+
+
+def sub_plots(data_frame, attr1, attr2, bins1, bins2):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
     fig.suptitle("Distributions of " + attr1 + " and " + attr2 + " in the dataset", fontsize = 16)
-    ax1.hist(table[attr1])
+    ax1.hist(data_frame[attr1],  bins=bins1)
     ax1.set_xlabel(attr1, fontsize = 13)
     ax1.set_ylabel("Frequency", fontsize = 13)
-    ax2.hist(table[attr2])
+    ax2.hist(data_frame[attr2], bins=bins2)
     ax2.set_xlabel(attr2, fontsize = 13)
     ax2.set_ylabel("Frequency", fontsize = 13);
     plt.show()
+

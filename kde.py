fc5a08799bf31ef726416af3aac005c10cbe66f2
Hadassah <ilhadassab@gmail.com>
2020-06-22 11:21:25 +0300

    find BW for kde

--- kde.py
+++ kde.py
@@ -1,9 +1,18 @@
-from sklearn.grid_search import GridSearchCV
-from sklearn.cross_validation import LeaveOneOut
+from pandas import np
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import LeaveOneOut
+from sklearn.neighbors import KernelDensity
 
-bandwidths = 10 ** np.linspace(-1, 1, 100)
-grid = GridSearchCV(KernelDensity(kernel='gaussian'),
-                    {'bandwidth': bandwidths},
-                    cv=LeaveOneOut(len(x)))
-grid.fit(x[:, None]);
-grid.best_params_
+def grid(data_frame):
+    """bandwidths = 10 ** np.linspace(-1, 1, 100)
+    grid = GridSearchCV(KernelDensity(kernel='gaussian'),
+                        {'bandwidth': bandwidths},
+                        cv=LeaveOneOut())
+    #grid.fit([:, None]);
+    grid.best_params_"""
+    data = data_frame.groupby('pls_name')[['latitude', 'longitude']]
+    # use grid search cross-validation to optimize the bandwidth
+    params = {'bandwidth': np.logspace(-1, 1, 20)}
+    grid = GridSearchCV(KernelDensity(), params)
+    grid.fit(data)
+    print("best bandwidth: {0}".format(grid.best_estimator_.bandwidth))

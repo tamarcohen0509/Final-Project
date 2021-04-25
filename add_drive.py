066c6af522cbf4955137139787fb42eef3cde889
Hadassah <ilhadassab@gmail.com>
2021-04-26 01:32:57 +0300

    final code for BETA assignment.

--- add_drive.py
+++ add_drive.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 counter = 0
 clusters = {}
+num_cluster = -1
 
 """
 input: traces
@@ -15,27 +16,9 @@ output: roads that are a clustering of this
     the clustes aren't on the original traces it's on the common parts of the traces.
 """
 def create_streets(traces):
-    roads = pd.DataFrame()
-    rides_to_cluster = traces[:]
-    j = 0
-    for road in rides_to_cluster:
-        # this loop creates a new cluster for each dot that does not belong to any
-        print(road)
-        for ind, dot in road.iterrows():
-            if road.loc[ind].cluster is None: # if the dot does not belong to any road
-                j += 1
-                road.loc[ind, 'cluster'] = j
-                # create a new cluster key in clusters dict
-                clusters[j] = {dot['pls_name']:[road.loc[ind]]}
-
-        for trace in traces:
-            if (trace is road): #if the trace is the main road which is the original
-                continue
-            check_trace_relation(road, trace)
-            plot_cluster()
-            merge_points_in_clusters()
-            plt.show()
-    return roads
+    for trace in traces:
+        check_trace_relation(trace)
+    merge_points_in_clusters()
 
 """
 input: trace and a list of tuples of index's where the trace
@@ -61,28 +44,37 @@ avg with the right points in trace and returns
 a list with tuples of index's where trace wasn't
 a match with road
 """
-def check_trace_relation(road, segment):
+def check_trace_relation(segment):
     width_trace = 0.1
     segment = pd.DataFrame(segment)
     if segment.empty:
         return None, None
     for ind, dot in segment.iterrows():
-        distance, index = find_the_perfect_index(road, (dot.latitude, dot.longitude))
-        segment.loc[ind,'check'] = add_dot_to_road(width_trace, distance) #belong dot only if it is in the road width
+        distance, index = find_the_perfect_index( (dot.latitude, dot.longitude, dot.pls_name))
+        if (index != -1):
+            segment.loc[ind,'check'] = add_dot_to_road(width_trace, distance) #belong dot only if it is in the road width
 
 
         if segment.loc[ind,'check']:
-            cluster = add_to_cluster(road, index, dot)
+            cluster = add_to_cluster(index, dot)
             segment.loc[ind, 'cluster'] = cluster
             clusters[cluster].setdefault(dot['pls_name'], []).append(dot)
+            merge_points_in_cluster(clusters[cluster])
+        else:
+            global num_cluster
+            num_cluster+= 1
+            segment.loc[ind, 'cluster'] = num_cluster
+            # create a new cluster key in clusters dict
+            clusters[num_cluster] = {dot['pls_name']: [segment.loc[ind]]}
+            clusters[num_cluster]['avg_point'] = (segment.loc[ind]['latitude'], segment.loc[ind]['longitude'])
 
 
 
-def add_to_cluster(road, index, dot):
+def add_to_cluster(index, dot):
     if dot.cluster is not None:
         print("dot has already a cluster it belongs to")
     else:
-        dot.loc['cluster'] = road.loc[index].cluster
+        dot.loc['cluster'] = index
     return dot.loc['cluster']
 
 """
@@ -108,22 +100,34 @@ def unchecked_segments(trace):
 
 """this function will be written by Tamar
  it returns the distance between dot and main_trace
-  and the index of the dot in main trace 
+  and the indefind_the_perfect_indexx of the dot in main trace 
   which is closest to the place in spline 
   where the distance was measured from
 """#horizontal and vertical are regarding to the vertivcal beween dot and trace herefore not sure if need this recursive
-def find_the_perfect_index(main_trace, dot):#point, df_segment):
-        min_distance = 1000000  # initial value
-        for index, row in main_trace.iterrows():
-            row_lat = row['latitude']
-            row_long = row['longitude']
+def find_the_perfect_index(dot):
+    min_distance = 1000000  # initial value
+    min_index = -1
+    for index, item in clusters.items():
+        if 'avg_point' in item:
+            row_lat = item['avg_point'][0]
+            row_long = item['avg_point'][1]
+            print('---->>>avg', (row_lat, row_long), 'index', index)
             tmp_distance = math_functions.__distance(dot, (row_lat, row_long))
+            print("tmp distance = ", tmp_distance)
             if tmp_distance < min_distance:
-                min_distance = tmp_distance
-                min_index = index
+                if not if_road_in_cluster(item, dot[2]):
+                    min_distance = tmp_distance
+                    min_index = index
+    return (min_distance, min_index)
 
-        return (min_distance, min_index)
-
+def if_road_in_cluster(cluster, pls_name):
+    print("FUNC", pls_name)
+    for pls in cluster:
+        if (pls == "avg_point"):
+            continue
+        if pls == pls_name:
+            return True
+    return False
 """
 This function adds a given dot to a road if it matches-
 if the distance is smaller then the width of the street.
@@ -190,6 +194,8 @@ using geometric avg. this will give us one road
 def merge_points_in_cluster(cluster):
     cluster_list = []
     for pls in cluster:
+        if(pls == "avg_point"):
+            continue
         if len(cluster[pls]) > 1:
             point_to_add = get_avg_in_pls(cluster[pls])
         else:
@@ -199,15 +205,14 @@ def merge_points_in_cluster(cluster):
             point_alt = point['altitude']
             point_to_add = (point_lat, point_long, point_alt)
         cluster_list.append(point_to_add)
-    avg_point = math_functions.__get_avg_point(cluster_list)
+        avg_point = math_functions.__get_avg_point(cluster_list)
+        cluster['avg_point'] = avg_point
     return avg_point
-
 
 def merge_points_in_clusters():
     avg_points = []
     for cluster in clusters.values():
         if len(cluster) < 2:
             continue
-        avg_point = merge_points_in_cluster(cluster)
-        avg_points.append(avg_point)
-        export_table.__plot_data_points(avg_point[0], avg_point[1], color='#000000', m_color='#000000', marker='X')
+        avg_points.append(cluster['avg_point'])
+        export_table.__plot_data_points(cluster['avg_point'][0], cluster['avg_point'][1], color='#000000', m_color='#000000', marker='X')
